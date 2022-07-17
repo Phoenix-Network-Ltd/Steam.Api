31872be391b924a2e49cedd882f47436bb37978a
Matthias Hoste <42743095+matthias-hoste@users.noreply.github.com>
2022-07-17 20:46:15 +0200

    Update SteamHelper.cs

--- Steam.Api/Helpers/SteamHelper.cs
+++ Steam.Api/Helpers/SteamHelper.cs
@@ -5,15 +5,16 @@ namespace Steam.Api.Helpers
 {
     public class SteamHelper
     {
-        public static async Task<Dictionary<string, object>> GetUser(string ticket)
+        private static string _publisherKey = Environment.GetEnvironmentVariable("STEAM_PUBLISHER_KEY");
+        public static async Task<Dictionary<string, object>> GetUser(string ticket, UInt64 appId)
         {
             using (var httpClient = new HttpClient())
             {
-                var request = new HttpRequestMessage(new HttpMethod("GET"), "https://partner.steam-api.com/ISteamUserAuth/AuthenticateUserTicket/v1/?key=PUTKEY&appid=PUTAPPID&ticket=" + ticket);
+                var request = new HttpRequestMessage(new HttpMethod("GET"), "https://partner.steam-api.com/ISteamUserAuth/AuthenticateUserTicket/v1/?key=" + _publisherKey + "&appid=" + appId + "&ticket=" + ticket);
                 var response = await httpClient.SendAsync(request);
                 var responseData = await response.Content.ReadAsStringAsync();
                 var responseDict = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Dictionary<string, object>>>>(responseData);
-                if (responseDict["response"].ContainsKey("error"))
+                if (responseDict == null || responseDict["response"].ContainsKey("error"))
                     return null;
 
                 return responseDict["response"]["params"];
