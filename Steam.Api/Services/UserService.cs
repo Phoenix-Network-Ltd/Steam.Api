f1a1ce0e1d81e4ff7eeacb3f7b8d56139a07042b
Matthias Hoste <42743095+matthias-hoste@users.noreply.github.com>
2022-07-17 17:07:49 +0200

    Return user

--- Steam.Api/Services/UserService.cs
+++ Steam.Api/Services/UserService.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using Grpc.Core;
 using Rpc;
+using Steam.Api.Helpers;
 
 namespace Steam.Api.Services
 {
 	public class UserService : User.UserBase
 	{
-        public override Task<UserResponse> GetWithTicket(GetWithTicketRequest request, ServerCallContext context)
+        public override async Task<UserResponse> GetWithTicket(GetWithTicketRequest request, ServerCallContext context)
         {
-            return base.GetWithTicket(request, context);
+            var response = await SteamHelper.GetUser(request.Ticket);
+
+            return new UserResponse
+            {
+                Success = response != null,
+                Id = (response == null ? 0 : Convert.ToUInt64(response["steamid"])),
+            };
         }
     }
 }
