dd16100ea5dc98da335cc94f6116a88b6ac9e254
Matthias Hoste <42743095+matthias-hoste@users.noreply.github.com>
2022-07-17 17:39:13 +0200

    Add transaction service

---
+++ Steam.Api/Services/TransactionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Grpc.Core;
+using Rpc;
+
+namespace Steam.Api.Services
+{
+    public class TransactionService : Transaction.TransactionBase
+    {
+        public override Task<CreateTransactionResponse> Create(CreateTransactionRequest request, ServerCallContext context)
+        {
+            return base.Create(request, context);
+        }
+    }
+}
